b8651cc056af87ee95c0704cc4226e4d3fc97525
Yusein Kerdakov <108539299+yuseinkerdakov@users.noreply.github.com>
2022-12-30 13:09:31 +0200

    Code refactoring (#3)
    * add helper function for suplements
    
    * create clothes helper functions
    
    * make functions exportable
    
    * add supplements info in supplements' helper function
    
    * make a separated function for supplements' map
    
    * helper files up
    
    * get the helper functions back
    
    * fix discount prices
    
    * made files to store values, textKeys, as well as helper files and made the SupplementsProvider work
    
    * remove unnecessary scripts and add a comment for the newly added one
    
    * moved the add to cart logic to separated file
    
    * add transform to all the products and change some comments, variables names
    
    * changes
    
    * updates!
    
    * resolve conflicts
    
    * add constants.
    
    * add cart items counter
    
    * css value changes
    
    * handle color change when hovering the cart icon
    
    * made separated page files for all pages in the app as well as reusable header and footer
    
    * back functionality and save progress - work in progress
    
    * last refactor changes
    
    * added deleting functionality in the cart
    
    * changes to not use the allProducts' last el data and already defined consts instead
    
    * css changes
    
    * css changes
    
    * add notifications, close cart button and counter max number
    
    Co-authored-by: Kevin Baliev <surprice222@gmail.com>

--- constants/StrongHardCodedValues.js
+++ constants/StrongHardCodedValues.js
@@ -1,8 +1,12 @@
-//in this file we want to add all the values with good namings so we can use them across the app
-//we don't really want to see strings anywhere in the project, right?
-//hardcoded values are bad practice.
-
 //Supplements & Clothes helpers' element classNames values
 export const NEW_BOX_ELEMENT_CLASSNAME = "box";
 export const PRICE_DIV_CLASSNAME = "price";
 export const ADD_TO_CART_BUTTON_CLASSNAME = "btn";
+export const ADD_TO_CART_BUTTON_CLASSNAME_2 = "add-to-cart";
+
+//Cart
+export const CART_ITEMS_COUNTER_CLASSNAME = "cart-items-counter";
+export const CART_CHECKOUT_BUTTON_DISABLED_CLASSNAME = "disabled";
+export const CART_EMPTY_CART_CLASSNAME_HIDDEN = "hiddenEmptyCart";
+export const CART_DELETE_BUTTON_CLASSNAME = "delete-btn";
+export const CART_ITEM_PRICE_CLASSNAME = "price";
--- js/AddToCartHandler.js
+++ js/AddToCartHandler.js
@@ -1,14 +1,27 @@
+import {
+  CART_CHECKOUT_BUTTON_DISABLED_CLASSNAME,
+  CART_DELETE_BUTTON_CLASSNAME,
+  CART_EMPTY_CART_CLASSNAME_HIDDEN,
+  CART_ITEMS_COUNTER_CLASSNAME,
+  CART_ITEM_PRICE_CLASSNAME,
+} from "../constants/StrongHardCodedValues.js";
+import {
+  cartCheckoutButtonTextKey,
+  cartItemDeleteButtonTextKey,
+} from "../constants/TextKeys.js";
+
 export const AddToCartHandler = () => {
   let allProducts = [];
   const checkOutButton = document.getElementById("checkOut");
   const cartIcon = document.getElementById("cart-btn");
   const productsCounter = document.createElement("div");
   cartIcon.appendChild(productsCounter);
-  productsCounter.className = "cart-items-counter";
+  productsCounter.className = CART_ITEMS_COUNTER_CLASSNAME;
 
   productsCounter.innerText = allProducts.length;
 
-  !allProducts.length && checkOutButton.classList.add("disabled");
+  !allProducts.length &&
+    checkOutButton.classList.add(CART_CHECKOUT_BUTTON_DISABLED_CLASSNAME);
   !allProducts.length && checkOutButton.setAttribute("disabled", "");
 
   const getTotalPrice = () => {
@@ -24,7 +37,6 @@ export const AddToCartHandler = () => {
   };
   let buttons = document.querySelectorAll(".box .btn");
 
-  //Add EventListeners
   checkOutButton && checkOutButton.addEventListener("click", getTotalPrice);
 
   buttons.forEach((btn) => {
@@ -33,50 +45,87 @@ export const AddToCartHandler = () => {
       const img = btn.previousSibling.previousSibling.previousSibling.src;
       const price = btn.previousSibling.firstChild.textContent;
       const id = Math.random() * Math.random();
+
       allProducts.push({ name: name, img: img, price: price, id: id });
+
+      const showNotification = notifier.show(
+        "✅ Cart update",
+        `${name} added to your cart!`,
+        "success",
+        "",
+        "",
+        0
+      );
+
+      setTimeout(() => notifier.hide(showNotification), 2500);
+
       document
         .getElementById("emptyCartTitle")
-        .classList.add("hiddenEmptyCart");
-      checkOutButton.textContent = "Check Out";
+        .classList.add(CART_EMPTY_CART_CLASSNAME_HIDDEN);
+
+      checkOutButton.textContent = cartCheckoutButtonTextKey;
+
       const cartItemsParent = document.querySelector(".cart-items-container");
       const itemToAdd = document.createElement("div");
+
       itemToAdd.setAttribute("id", id);
       itemToAdd.classList.add("cart-item");
+
       const deleteButton = document.createElement("span");
       const itemImg = document.createElement("img");
       const divContent = document.createElement("div");
       divContent.classList.add("content");
+
       const itemName = document.createElement("h3");
       const itemPrice = document.createElement("div");
-      itemPrice.classList.add("price");
+      itemPrice.classList.add(CART_ITEM_PRICE_CLASSNAME);
+
       itemToAdd.appendChild(deleteButton);
-      deleteButton.innerText = "X";
-      deleteButton.className = "delete-btn";
+      deleteButton.innerText = cartItemDeleteButtonTextKey;
+      deleteButton.className = CART_DELETE_BUTTON_CLASSNAME;
+
       cartItemsParent.appendChild(itemToAdd);
       itemToAdd.appendChild(itemImg);
       itemToAdd.appendChild(divContent);
+
       divContent.appendChild(itemName);
       divContent.appendChild(itemPrice);
-      productsCounter.innerText = allProducts.length;
+
+      if (allProducts.length < 10) {
+        productsCounter.innerText = allProducts.length;
+      }
+      if (allProducts.length >= 10) {
+        productsCounter.innerText = "9+";
+      }
       itemName.textContent = name;
       itemImg.src = img;
       itemPrice.textContent = price;
+
       cartItemsParent &&
         cartItemsParent.insertBefore(itemToAdd, cartItemsParent.children[0]);
+
       deleteButton.addEventListener("click", () => {
         allProducts = allProducts.filter((item) => item.id !== id);
-        checkOutButton.textContent = "Check Out";
-        productsCounter.innerText = allProducts.length;
+        checkOutButton.textContent = cartCheckoutButtonTextKey;
+        if (allProducts.length < 10) {
+          productsCounter.innerText = allProducts.length;
+        }
+        if (allProducts.length >= 10) {
+          productsCounter.innerText = "9+";
+        }
         document.getElementById(id).remove();
         !allProducts.length && checkOutButton.classList.add("disabled");
         !allProducts.length && checkOutButton.setAttribute("disabled", "");
         !allProducts.length &&
           document
             .getElementById("emptyCartTitle")
-            .classList.remove("hiddenEmptyCart");
+            .classList.remove(CART_EMPTY_CART_CLASSNAME_HIDDEN);
       });
 
-      allProducts.length && checkOutButton.classList.remove("disabled");
+      allProducts.length &&
+        checkOutButton.classList.remove(
+          CART_CHECKOUT_BUTTON_DISABLED_CLASSNAME
+        );
       allProducts.length && checkOutButton.removeAttribute("disabled", "");
     });
   });
